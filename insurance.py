8bd3d29250ea6c2fe30847878790163f3155b9d1
Etash Jhanji <etashjhanji@gmail.com>
2025-12-01 12:34:54 -0500

    Add normalization for insurance data

--- insurance.py
+++ insurance.py
@@ -2,6 +2,7 @@ import pandas as pd
 from MultipleRegression import regress
 from Grapher import plotMultipleRegression as plot
 from Grapher import linearToString
+from sklearn.preprocessing import MinMaxScaler
 
 # Read the dataset in
 insurance_df = pd.read_csv("insurance_dataset.csv")
@@ -13,10 +14,13 @@ insurance_df = insurance_df.drop(insurance_df[insurance_df.Smoker == "yes"].inde
 df = insurance_df.drop(['Smoker', 'Number_of_Children', 'Gender', 'Region', ], axis=1) 
 # New dataframe Has ["Age", "BMI", "Insurance_Cost"] for nonsmokers
 
+scaler = MinMaxScaler()
+df_normalized_minmax = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+
 # 2 Independent Variables
-X = df[['Age', 'BMI']].to_numpy()
+X = df_normalized_minmax[['Age', 'BMI']].to_numpy()
 # 1 Dependent Variable
-Y = df["Insurance_Cost"].to_numpy()
+Y = df_normalized_minmax["Insurance_Cost"].to_numpy()
 
 # Regress, print, and plot
 R = regress(X, Y)
